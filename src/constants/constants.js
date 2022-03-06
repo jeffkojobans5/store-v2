ad9854306d12cd4b6e4ee0fb8d3a878ee6f06f53
Jeff <iambansah@gmail.com>
2022-03-06 03:37:16 +0000

    made objects for  social media and and nav links

---
+++ src/constants/constants.js
@@ -0,0 +1,43 @@
+const social_media = [
+    {
+        id : 1,
+        name : "facebook",
+        url : "fb.com",
+        icon : "<FaBook/>"
+    },
+    {
+        id : 2,        
+        name : "facebook",
+        url : "fb.com",
+        icon : "<FaBook/>"        
+    },
+    {
+        id : 3,        
+        name : "facebook",
+        url : "fb.com",
+        icon : "<FaBook/>"        
+    },    
+]
+
+const navbar_links = [
+    {
+        id : 1,
+        name : 'home',
+        link : '/'
+    },
+    {
+        id : 2,
+        name : 'about',
+        link : '/'
+    },    
+    {
+        id : 3,
+        name : 'shop',
+        link : '/'
+    },       
+    {
+        id : 4,
+        name : 'contact',
+        link : '/'
+    },       
+]
